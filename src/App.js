463e1a3503103547858f74402986d3d07f59870f
Bobby Caya <bob.caya1@gmail.com>
2018-06-13 17:39:26 -0600

    add props and event listeners

--- src/App.js
+++ src/App.js
@@ -2,15 +2,16 @@ import React from 'react';
 import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
-import { callbackify } from 'util';
 
 
-// const API_KEY = 'API_KEY';
-const city ="city"
-const country="country"
+const API_KEY = 'API_KEY';
+
 class App extends React.Component {
 
-  getWeather = async () => {
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.name.city.value;
+    const country = e.target.elements.name.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
   }
@@ -18,7 +19,7 @@ class App extends React.Component {
     return (
       <div>
         <Titles />
-        <Form />
+        <Form getWeather={this.getWeather} />
         <Weather />
       </div>
     )
--- src/components/Form.js
+++ src/components/Form.js
@@ -4,9 +4,10 @@ class Form extends React.Component{
   render(){
     return(
       <div>
-       <form>
+       <form onSubmit={this.props.getWeather}>
         <input type="text" name="city" placeholder="City..." />
         <input type="text" name="country" placeholder="Country..." />
+        <button>Get Weather</button>
        </form>
       </div>
     )
