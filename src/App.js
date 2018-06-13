f66255404d60d36a214c7499196a8186bdec15c3
Bobby Caya <bob.caya1@gmail.com>
2018-06-13 17:17:54 -0600

    add api key example

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
+
+const API_KEY = "API_KEY"
 class App extends React.Component {
   render() {
     return (
