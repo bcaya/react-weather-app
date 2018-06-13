a1a1244d436464a96eae22f73e2766e44d2aca41
Bobby Caya <bob.caya1@gmail.com>
2018-06-13 17:29:25 -0600

    Add form component and fetch call

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import React from 'react';
 import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
+import { callbackify } from 'util';
 
-const API_KEY = "API_KEY"
+
+// const API_KEY = 'API_KEY';
+const city ="city"
+const country="country"
 class App extends React.Component {
+
+  getWeather = async () => {
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
+    const data = await api_call.json();
+  }
   render() {
     return (
       <div>
