6e886de81ba5434cc66f8a5f8ceaaba1994cdcc5
Bobby Caya <bob.caya1@gmail.com>
2018-06-13 17:49:53 -0600

    set state for city json return information.

--- src/App.js
+++ src/App.js
@@ -3,17 +3,34 @@ import Titles from './components/Titles';
 import Form from './components/Form';
 import Weather from './components/Weather';
 
-
 const API_KEY = 'API_KEY';
 
 class App extends React.Component {
 
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
   getWeather = async (e) => {
     e.preventDefault();
-    const city = e.target.elements.name.city.value;
-    const country = e.target.elements.name.country.value;
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description
+
+
+    })
   }
   render() {
     return (
