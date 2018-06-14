569c46a4aefa1db35eed6898f7321ba60bf5a894
Bobby Caya <bob.caya1@gmail.com>
2018-06-13 18:10:13 -0600

    error handle for if there is no input

--- src/App.js
+++ src/App.js
@@ -23,15 +23,25 @@ class App extends React.Component {
     const country = e.target.elements.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
-    this.setState({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description
-
-
-    })
+    if ( city && country )
+      {this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description
+      })
+    } else {
+      {this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter a place"
+      })
+      }
+    }
   }
   render() {
     return (
